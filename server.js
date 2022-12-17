a714a48de3dc9e6f7d1301395970b28b1d7c301c
Emin Çoşkun <hyklops@hotmail.com>
2022-12-17 18:37:16 +0300

    update

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import cors from "cors";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://www.omdbapi.com/?",
+  })
+);
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
