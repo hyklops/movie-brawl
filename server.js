fb60e53277bfd829213dd72672ede17a6f061b34
Emin Çoşkun <hyklops@hotmail.com>
2022-12-19 14:18:48 +0300

    deploying on vercel

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
-import { express } from "express";
-import cors from "cors";
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
 
-const app = express();
+const handler = (req, res) => {
+  const d = new Date();
+  res.end(d.toString());
+};
 
-app.use(
-  cors({
-    origin: "http://www.omdbapi.com/?",
-  })
-);
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
+module.exports = allowCors(handler);
